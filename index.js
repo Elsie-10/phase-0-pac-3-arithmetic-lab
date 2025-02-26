5a7a3f617d5fe56cca2d9df173a45a27c2f72617
Elsie <elsieoscar06@gmail.com>
2025-02-26 23:21:33 +0300

    arithmetic-lab

---
+++ index.js
@@ -0,0 +1,29 @@
+
+function add(a , b){
+    // we'll fill this in shortly.
+    return (a + b);
+}
+function subtract(a, b){
+return a - b;
+}
+function multiply(a, b){
+return a * b;
+}
+function divide(a , b){
+return (a/b);
+}
+function increment(n){
+    return n + 1;
+}
+function decrement(n){
+    return n - 1;
+}
+// Converts a string to a base 10 integer
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+
+// Converts a string to a float (preserves the decimal part)
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
